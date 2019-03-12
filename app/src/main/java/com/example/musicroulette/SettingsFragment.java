57b69b99415d6d91bdbb93b62a040df3611cf1e0
Vincent Ta <tav@oregonstate.edu>
2019-03-12 15:00:07 -0700

    Add genre pref selection in settings

--- app/src/main/java/com/example/musicroulette/SettingsFragment.java
+++ app/src/main/java/com/example/musicroulette/SettingsFragment.java
@@ -4,6 +4,14 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.preference.PreferenceFragmentCompat;
 
-public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+public class SettingsFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
+    @Override
+    public void onCreatePreferences(Bundle bundle, String s) {
+        addPreferencesFromResource(R.xml.prefs);
+    }
 }
