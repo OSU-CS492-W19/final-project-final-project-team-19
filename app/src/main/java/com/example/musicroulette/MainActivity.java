e0f24b2a9e689a03b13a54dcaa48d94dd2f96e0f
jimweldev <aguinaji@oregonstate.edu>
2019-03-12 01:41:20 -0700

    Fixed Authentication. Once the main activity starts, a login page should pop up for the user to sign in with their spotify account. Once logged in, an authorization token will be returned.

--- app/src/main/java/com/example/musicroulette/MainActivity.java
+++ app/src/main/java/com/example/musicroulette/MainActivity.java
@@ -1,29 +1,64 @@
 package com.example.musicroulette;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+
 import com.example.musicroulette.utils.SpotifyUtils;
+import com.spotify.sdk.android.authentication.AuthenticationClient;
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
+    private String mAccessToken;
+    private String mAccessCode;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        requestAuthorizatonFromSpotify();
     }
 
-    private void requestAuthorizatonFromSpotify() {
-        String url = SpotifyUtils.buildAuthorizationURL();
-        Log.d(TAG, "Requesting authorization at: " + url);
-        new SpotifyAuthorizationTask().execute(url);
+    @Override
+    protected void onStart() {
+        super.onStart();
+        RequestCode();
+        RequestToken();
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    public void RequestCode() {
+        final AuthenticationRequest request = SpotifyUtils.getAuthenticationRequest(AuthenticationResponse.Type.CODE);
+        AuthenticationClient.openLoginActivity(this, SpotifyUtils.AUTH_CODE_REQUEST_CODE, request);
+    }
+
+    public void RequestToken() {
+        final AuthenticationRequest request = SpotifyUtils.getAuthenticationRequest(AuthenticationResponse.Type.TOKEN);
+        AuthenticationClient.openLoginActivity(this, SpotifyUtils.AUTH_TOKEN_REQUEST_CODE, request);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        final AuthenticationResponse response = AuthenticationClient.getResponse(resultCode, data);
+
+        if (SpotifyUtils.AUTH_TOKEN_REQUEST_CODE == requestCode) {
+            mAccessToken = response.getAccessToken();
+            Log.d(TAG, "Access Token: " + mAccessToken);
+        } else if (SpotifyUtils.AUTH_CODE_REQUEST_CODE == requestCode) {
+            mAccessCode = response.getCode();
+            Log.d(TAG, "Access Code: " + mAccessCode);
+        }
+    }
 
 }
--- app/src/main/java/com/example/musicroulette/utils/SpotifyUtils.java
+++ app/src/main/java/com/example/musicroulette/utils/SpotifyUtils.java
@@ -2,6 +2,9 @@ package com.example.musicroulette.utils;
 
 import android.net.Uri;
 
+import com.spotify.sdk.android.authentication.AuthenticationRequest;
+import com.spotify.sdk.android.authentication.AuthenticationResponse;
+
 import okhttp3.MultipartBody;
 import okhttp3.Request;
 import okhttp3.RequestBody;
@@ -10,49 +13,19 @@ public class SpotifyUtils {
 
     private final static String TAG = SpotifyUtils.class.getSimpleName();
 
-    private final static String SPOTIFY_CLIENT_ID = "a8a1ba77b43d4c6abca5c15a7129bf44";
-    private final static String SPOTIFY_CLIENT_SECRET = "a8a1ba77b43d4c6abca5c15a7129bf44";
-    private final static String SPOTIFY_REDIRECT_URI = "https://musicroulette.com/spotify-redirect";
+    public final static String CLIENT_ID = "a8a1ba77b43d4c6abca5c15a7129bf44";
+    public final static String CLIENT_SECRET = "a8a1ba77b43d4c6abca5c15a7129bf44";
+    public final static String REDIRECT_URI = "musicroulette://spotify";
 
-    private final static String SPOTIFY_AUTHORIZATION_BASE_URL = "https://accounts.spotify.com/authorize";
-    private final static String SPOTIFY_AUTHORIZATION_STATE = "W33SrNUliN";
-    private final static String SPOTIFY_AUTHORIZATION_RESPONSE_TYPE = "code";
-    private final static String SPOTIFY_AUTHORIZATION_CLIENT_ID_PARAM = "client_id";
-    private final static String SPOTIFY_AUTHORIZATION_RESPONSE_TYPE_PARAM = "response_type";
-    private final static String SPOTIFY_AUTHORIZATION_REDIRECT_URI_PARAM = "redirect_uri";
+    public final static int AUTH_TOKEN_REQUEST_CODE = 0x10;
+    public final static int AUTH_CODE_REQUEST_CODE = 0x11;
 
-    public static String buildAuthorizationURL() {
-        return Uri.parse(SPOTIFY_AUTHORIZATION_BASE_URL).buildUpon()
-                .appendQueryParameter(SPOTIFY_AUTHORIZATION_CLIENT_ID_PARAM, SPOTIFY_CLIENT_ID)
-                .appendQueryParameter(SPOTIFY_AUTHORIZATION_RESPONSE_TYPE_PARAM, SPOTIFY_AUTHORIZATION_RESPONSE_TYPE)
-                .appendQueryParameter(SPOTIFY_AUTHORIZATION_REDIRECT_URI_PARAM, SPOTIFY_REDIRECT_URI)
-                .build()
-                .toString();
-    }
-
-    private final static String SPOTIFY_REFRESH_TOKENS_BASE_URL = "https://accounts.spotify.com/api/token";
-    private final static String SPOTIFY_REFRESH_TOKENS_GRANT_TYPE_PARAM = "grant_type";
-    private final static String SPOTIFY_REFRESH_TOKENS_CODE_PARAM = "code";
-    private final static String SPOTIFY_REFRESH_TOKENS_REDIRECT_URI_PARAM = "redirect_uri";
-    private final static String SPOTIFY_REFRESH_TOKENS_CLIENT_ID_PARAM = "client_id";
-    private final static String SPOTIFY_REFRESH_TOKENS_CLIENT_SECRET_PARAM = "client_secret";
-    private final static String SPOTIFY_REFRESH_TOKENS_GRANT_TYPE = "authorization_code";
-
-    public static Request requestAccessandRefreshTokens(String code) {
-        RequestBody requestBody = new MultipartBody.Builder()
-                .setType(MultipartBody.FORM)
-                .addFormDataPart(SPOTIFY_REFRESH_TOKENS_GRANT_TYPE_PARAM, SPOTIFY_REFRESH_TOKENS_GRANT_TYPE)
-                .addFormDataPart(SPOTIFY_REFRESH_TOKENS_CODE_PARAM, code)
-                .addFormDataPart(SPOTIFY_REFRESH_TOKENS_REDIRECT_URI_PARAM, SPOTIFY_REDIRECT_URI)
-                .addFormDataPart(SPOTIFY_REFRESH_TOKENS_CLIENT_ID_PARAM, SPOTIFY_CLIENT_ID)
-                .addFormDataPart(SPOTIFY_REFRESH_TOKENS_CLIENT_SECRET_PARAM, SPOTIFY_CLIENT_SECRET)
+    public static AuthenticationRequest getAuthenticationRequest(AuthenticationResponse.Type type) {
+        return new AuthenticationRequest.Builder(SpotifyUtils.CLIENT_ID,
+                type, SpotifyUtils.REDIRECT_URI)
+                .setShowDialog(false)
+                .setScopes(new String[]{"streaming"})
+                .setCampaign("your-campaign-token")
                 .build();
-
-        Request request = new Request.Builder()
-                .url(SPOTIFY_REFRESH_TOKENS_BASE_URL)
-                .post(requestBody)
-                .build();
-
-        return request;
     }
 }
